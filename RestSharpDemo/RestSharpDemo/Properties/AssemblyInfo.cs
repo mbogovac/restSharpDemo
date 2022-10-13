5ee2f7ac6cacfdb745d6be966e974902dfbefae0
milabogovac <bogovac.mila24@gmail.com>
2022-10-13 13:44:18 +0200

    first get and post tests

---
+++ RestSharpDemo/RestSharpDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Levi9")]
+[assembly: AssemblyProduct("RestSharpDemo")]
+[assembly: AssemblyCopyright("Copyright © Levi9 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8fe3c9aa-933c-4abd-b189-5aa787b432ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RestSharpDemo/RestSharpDemo/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using RestSharpDemo.Model;
+using System;
+
+namespace RestSharpDemo
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts/{postid}", Method.Get);
+            request.AddUrlSegment("postid", 1);
+
+            var response = client.Execute(request);
+
+            JObject obs = JObject.Parse(response.Content);
+            Assert.That(obs["author"].ToString(), Is.EqualTo("Karthik KK"), "Author is not correct");
+        }
+
+        [Test]
+        public void PostWithAnonymousClass()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts/{postid}/profile", Method.Post);
+
+            request.RequestFormat = DataFormat.Json;
+
+            request.AddBody(new { name = "Sam" });
+            request.AddUrlSegment("postid", 1);
+
+            var response = client.Execute(request);
+
+            JObject obs = JObject.Parse(response.Content);
+            Assert.That(obs["name"].ToString(), Is.EqualTo("Sam"), "Name is not correct");
+        }
+
+        [Test]
+        public void PostWithTypeClass()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("posts", Method.Post);
+
+            request.RequestFormat = DataFormat.Json;
+
+            request.AddBody(new Posts() { id = "13", author = "Del Boy", title = "RestSharp Course"});
+
+            var response = client.Execute(request);
+
+            JObject obs = JObject.Parse(response.Content);
+            Assert.That(obs["author"].ToString(), Is.EqualTo("Del Boy"), "Name is not correct");
+        }
+    }
+}
